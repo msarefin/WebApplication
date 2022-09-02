a30ca0a2612d734a00f917cec00a8406c0aa388c
msarefin <mohammed.arefin@gmail.com>
2022-09-02 17:53:17 -0400

    adding attribute to the order

--- javascript/practice.js
+++ javascript/practice.js
@@ -6,16 +6,49 @@ let c = 0;
 
 
 function increase(){
-    c++; 
-    console.log(c);
-    counter.innerHTML=c;
+    if(c>=0){
+        c++; 
+        console.log(c);
+        counter.innerHTML=c;
+        tableIns();
+        listItem();
+    }
 }
 
 function decrease(){
-    c--; 
-    console.log(c);
-    counter.innerHTML=c;
+    if(c>0){
+        c--; 
+        console.log(c);
+        counter.innerHTML=c;
+        tableIns();
+        listItem();
+    }
 }
 
 inc.addEventListener('click',increase)
-dec.addEventListener('click', decrease);
+dec.addEventListener('click',decrease);
+
+const mylist = document.getElementById('myList');
+
+function listItem(){
+        
+    
+        const li = document.createElement('li');
+        li.id = 'listItem'; 
+        li.setAttribute('data-counter',c);
+        const text = document.createTextNode('counter: '+c);
+        li.appendChild(text);
+        mylist.appendChild(li);
+}
+
+
+const tblBody = document.getElementById('tbl');
+
+function tableIns(){
+    const trow = document.createElement('tr');
+    trow.id='row';
+    const tcell = document.createElement('td');
+    tcell.id='cell';
+    const text = document.createTextNode('ID: '+c);
+    tblBody.appendChild(trow).appendChild(tcell).appendChild(text);
+}
