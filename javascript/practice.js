0988727323304b2d24173498d010e144a4c77fa2
msarefin <mohammed.arefin@gmail.com>
2022-09-02 01:30:49 -0400

    working on increasing and decreasing

--- javascript/practice.js
+++ javascript/practice.js
@@ -1,5 +1,21 @@
-let counter = document.querySelector('num');
-let inc = document.querySelector('inc');
-let dec = document.querySelector('dec');
+let counter = document.querySelector('#num');
+let inc = document.querySelector('#inc');
+let dec = document.querySelector('#dec');
+
+let c = 0;
 
 
+function increase(){
+    c++; 
+    console.log(c);
+    counter.innerHTML=c;
+}
+
+function decrease(){
+    c--; 
+    console.log(c);
+    counter.innerHTML=c;
+}
+
+inc.addEventListener('click',increase)
+dec.addEventListener('click', decrease);
